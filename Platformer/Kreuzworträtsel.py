aa71e9179c641d4a7f9b93e37d72a0bae2989260
DavidIsCool0072 <168551762+DavidIsCool0072@users.noreply.github.com>
2024-06-08 08:31:57 +0200

    Delete Platformer directory

--- Platformer/Kreuzworträtsel.py
+++
@@ -1,7 +0,0 @@
-import random
-abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-characters = "^1234567890ß´qwertzuiopü+asdfghjklöä#<yxcvbnm,.-°!\"§$%&/()=?`QWERTZUIOPÜ*ASDFGHJKLÖÄ'>YXCVBNM;:_„¡“¶¢[]|{}≠¿'"
-l = 0
-while l < 10000:
-    l += 1
-    print(random.choice(abc), end="")
